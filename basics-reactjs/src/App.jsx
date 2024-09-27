ef079ed1a420c2147a17c3d7523498716492960e
Savio Ferreira <saviofc.dev@gmail.com>
2024-09-26 22:37:47 -0300

    criando componente header e sideBar

--- basics-reactjs/src/App.jsx
+++ basics-reactjs/src/App.jsx
@@ -1,17 +1,18 @@
-import { Salve } from './Salve'
+import './global.css';
+import styles from './App.module.css'
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <>
-      <Salve
-        name="maconha com vinagre"
-        texto="dizem que é muito bom... acalma a alma dos mais necessitados e enfuridos de furor e rigosidade"
-      />
-
-      <Salve
-        name="Os loucos..."
-        texto="Só os loucos sabem desse grande segredo que foi enterrado com ele naquele dia. ahh, pobre alma "
-      />
-    </>
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h1>Salve salve</h1>
+        </main>
+      </div>
+    </div>
   )
 }
--- basics-reactjs/src/Salve.jsx
+++
@@ -1,9 +0,0 @@
-
-export function Salve(props) {
-  return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>{props.texto}</p>
-      </div>
-  )
-}
